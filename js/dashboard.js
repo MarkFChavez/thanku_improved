72fcdc268a502b00b9a36fa0f706b1646fbf5565
Mark Chavez <markchav3z@gmail.com>
2015-02-01 11:38:52 +0800

    Apply masonry on dashboard news feed

---
+++ js/dashboard.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(window).load(function() {
+    $("#cards").imagesLoaded(function() {
+      $("#cards").masonry({
+        columnWidth: 70,
+        itemSelector: ".card",
+        isOriginLeft: true
+      });
+    });
+  });
+});
